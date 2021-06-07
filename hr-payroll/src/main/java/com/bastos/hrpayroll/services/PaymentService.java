1bdd9d6aa20a152162907bb620c4daa597748f00
CaioBastosImpacta <caio.bastos@aluno.faculdadeimpacta.com.br>
2021-06-07 18:56:40 -0300

    implementando spring clound feing clients

--- hr-payroll/src/main/java/com/bastos/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/bastos/hrpayroll/services/PaymentService.java
@@ -2,29 +2,18 @@ package com.bastos.hrpayroll.services;
 
 import com.bastos.hrpayroll.entities.Payment;
 import com.bastos.hrpayroll.entities.Worker;
+import com.bastos.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
 
     @Autowired
-    RestTemplate restTemplate;
-
-    @Value("${hr-worker.host}")
-    String workerHost;
+    WorkerFeignClient workerFeignClient;
 
     public Payment getPayment(long workerId, int days) {
-        Map<String, String> uriVariables = new HashMap<>() {{
-            put("id", String.valueOf(workerId));
-        }};
-
-        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        Worker worker = workerFeignClient.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
