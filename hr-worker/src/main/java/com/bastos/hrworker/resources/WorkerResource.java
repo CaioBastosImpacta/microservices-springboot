f5ca8a113dc744bc26678ec201fc3c8fcd73e852
CaioBastosImpacta <caio.bastos@aluno.faculdadeimpacta.com.br>
2021-06-09 15:27:10 -0300

    hr-worker configurando o cliente de configurações do servidor

--- hr-worker/src/main/java/com/bastos/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/bastos/hrworker/resources/WorkerResource.java
@@ -5,6 +5,7 @@ import com.bastos.hrworker.repositories.WorkerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +21,9 @@ public class WorkerResource {
 
     private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 
+    @Value("${test.config}")
+    private String testConfig;
+
     @Autowired
     Environment environment;
 
@@ -30,6 +34,12 @@ public class WorkerResource {
     public ResponseEntity<List<Worker>> findAll() {
         List<Worker> workers = workerRepository.findAll();
         return ResponseEntity.ok(workers);
+    }
+
+    @GetMapping(value = "/configs")
+    public ResponseEntity<Void> getConfigs() {
+        logger.info("CONFIG = " + testConfig);
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping(value = "/{id}")
