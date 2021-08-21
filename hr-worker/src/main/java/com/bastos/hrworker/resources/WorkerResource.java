2165c93592f9e152b8e7f0b3b220ab96d0f7e01e
CaioBastosImpacta <caio.bastos@aluno.faculdadeimpacta.com.br>
2021-08-20 23:14:58 -0300

    configuracao do banco de dados no servidor

--- hr-worker/src/main/java/com/bastos/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/bastos/hrworker/resources/WorkerResource.java
@@ -5,7 +5,6 @@ import com.bastos.hrworker.repositories.WorkerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
@@ -23,9 +22,6 @@ public class WorkerResource {
 
     private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 
-    @Value("${test.config}")
-    private String testConfig;
-
     @Autowired
     Environment environment;
 
@@ -40,7 +36,6 @@ public class WorkerResource {
 
     @GetMapping(value = "/configs")
     public ResponseEntity<Void> getConfigs() {
-        logger.info("CONFIG = " + testConfig);
         return ResponseEntity.noContent().build();
     }
 
